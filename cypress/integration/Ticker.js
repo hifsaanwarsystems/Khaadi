6ad0e29c3a1ca39c9a2b69a56072fd2f7c9f15a6
hifsaanwarsystems <hifsa.anwar@systemsltd.com>
2022-03-15 23:12:29 +0500

    ticker done

--- cypress/integration/Ticker.js
+++ cypress/integration/Ticker.js
@@ -5,11 +5,27 @@ const getvalue=require('../fixtures/data.json')
 export class Ticker{
 
 
-tickerExist(){
+tickerExist()
+{
 
-cy.log('**[ Ticker Exists ]()**')
-cy.get(getvalue.tickerClass).should('exist')
+    cy.log('**[ Ticker Exists ]()**')
+    cy.get(getvalue.tickerClass).should('exist')
 }
 
+tickerText()
+{
+    cy.log('**[ Ticker Text and Css]()**')
+    cy.xpath(getvalue.tickerTextXpath).should('include.text',getvalue.tickerText).should('have.css','background-color',getvalue.tickerBackgroundColor).should('have.css','color',getvalue.tickerColor)
+
+}
+
+shopNow()
+{
+    cy.log('**[ Shop Now Text and Css]()**')
+    cy.get('span > a').should('exist').should('have.css','text-decoration',getvalue.shopNowCss)
+    cy.get('.ticker-text > span > a').invoke('removeAttr', 'target').click({force:true})
+    cy.url().should('include',getvalue.shopNowlink)
+    cy.go('back')
+}
 
 }
--- cypress/integration/main.js
+++ cypress/integration/main.js
@@ -11,13 +11,9 @@ describe('Khaadi Home Page',function(){
        
     })
     it('Testing Top Ticker',function(){
-ticker.tickerExist()
-        cy.log('**[ Ticker Text and Css]()**')
-        cy.xpath("//p[@class='ticker-text']").should('include.text',getvalue.tickerText).should('have.css','background-color',getvalue.tickerBackgroundColor).should('have.css','color',getvalue.tickerColor)
-
-        cy.log('**[ Shop Now]()**')
-        cy.get('.ticker-text > span > a').invoke('removeAttr', 'target').click({force:true})
-        cy.url().should('include','new-in.html')
+        ticker.tickerExist()
+        ticker.tickerText()
+        ticker.shopNow()
     })
 
     
