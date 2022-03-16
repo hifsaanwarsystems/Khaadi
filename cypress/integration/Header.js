7ac1d8e64940e40b8f54c44bdbecda6eb4cd856a
hifsaanwarsystems <hifsa.anwar@systemsltd.com>
2022-03-17 00:56:56 +0500

    done

--- cypress/integration/Header.js
+++ cypress/integration/Header.js
@@ -7,7 +7,7 @@ export class Header{
     logoCheck()
     {
       cy.log('**[ Checking Logo ]()**')
-        cy.get('.logo > img').should('exist').should('be.visible')
+      cy.get('.logo > img').should('exist').should('be.visible')
     }
 
     Lawn2022()
@@ -47,18 +47,19 @@ export class Header{
 
     NewIn()
     {
+      cy.log('**[ Hovering Over to New In Category ]()**')
       cy.contains("li.ui-menu-item","New In").realHover('mouse')
-       
       cy.xpath("(//div[@class='level0 submenu'])").should('be.visible').invoke('show')
+
+      cy.log('**[ Image Check Fabrics ]()**')
       cy.get('.menu-thumb-img ').eq(0).should('have.descendants','a')
       cy.get('.menu-thumb-link').eq(2).should('have.attr','href','https://pk.khaadi.com/new-in/unstitched.html').should('have.css','background').and('equal','rgba(0, 0, 0, 0) url("https://pk.khaadi.com//media/catalog/category/Thumbnail-2_2_11.jpg") no-repeat scroll 50% 0% / contain padding-box border-box')
 
+      cy.log('**[ Span Check Fabrics ]()**')
       cy.xpath("(//div[@class='level0 submenu']//span[contains(.,'Fabrics')])[2]").should('be.visible').click({force:true})
       cy.url().should('equal','https://pk.khaadi.com/new-in/unstitched.html')
       cy.go('back')
-      //cy.xpath("(//span[contains(.,'Fabrics')])[1]").should('be.visible')
-//cy.xpath("(//div[@class='level0 submenu']//span[contains(.,'Ready To Wear')])[1]").should('be.visible')
-        //cy.xpath("(//span[contains(.,'Western')])[1]").should('be.visible')
+   
     }
 
     fabrics()
